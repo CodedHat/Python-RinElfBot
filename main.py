b1289d11fe36677a9eb0edc30f79e9ee9568b169
TcpRinux <mattias.backhans@gmail.com>
2019-04-26 02:42:36 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import random
 from twython import Twython
+from pathlib import Path
 
 from auth import (
     consumer_key,
@@ -15,15 +16,16 @@ twitter = Twython(
 )
 
 
+messages = open('data.txt','r').read().replace('\n',' ')
 
-messages = [
-        "I like pie",
-        "I like pies, bitches!",
-        "I like my marshmallow bf",
-        "I like his butt...",
-]
-message = random.choice(messages)
+
+#messages = [
+#        "I like pie",
+#        "Error 404,not found",
+#        "I live in a small place, a small device called Raspberry Pi"
+#]
+
+messages = random.choice(messages)
 
 twitter.update_status(status=message)
 print("Tweeted: {}".format(message))
-
